ec79a9809592858275406d8a3e233abd07888de3
gofaadil <gofaadil@live.com>
2017-12-30 09:12:10 +0530

    uyyub

---
+++ ThePIMS_Hospital/GUI/Prescription/Presc_All.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ThePIMS_Hospital.GUI.Prescription
+{
+    /// <summary>
+    /// Interaction logic for Presc_All.xaml
+    /// </summary>
+    public partial class Presc_All : Window
+    {
+        public Presc_All()
+        {
+            InitializeComponent();
+        }
+    }
+}
